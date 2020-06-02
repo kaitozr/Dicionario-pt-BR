f030999e3143ba008d4c20f23b4cb7197cfd4b58
Ricardo Kaito <65265490+kaitozr@users.noreply.github.com>
2020-06-02 11:02:10 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ import xml.etree.ElementTree as ET
 import os
 import glob
 from unidecode import unidecode
+from tkinter import *
 ``` 
 - [x] Read all XML database files;
 - [x] Recognize words with different typing (accents, lower/upper cases);
 - [ ] Return all information (word, grammatical class, meaning) with legible formatting;
-- [ ] Create a GUI with tkinter
+- [x] Create a GUI with tkinter
+- [ ] Optimize tkinter GUI
 
 *About the coder:
 I'm a begginer in coding, python is my first language and this is my first project. So if you see something strange, just tell me, i'll be glad to take constructive critics.
--- importDic.py
+++ importDic.py
@@ -1,31 +1,57 @@
 import xml.etree.ElementTree as ET
 import os, glob
 from unidecode import unidecode
+from tkinter import *
 
-# Select the directory for the XML database
-path = os.getcwd() + '/xmls'
-allFiles = glob.glob(path+'/*.xml')
-#
 
-def word_comparator(): # Compare the received word from the input and the dictionary database
-    no_accent_RecWord = unidecode(received_word).upper() # Clean input in unidecode
-    noAccentDicWord = unidecode(dic_word).upper() # Clean database in unidecode
-    if no_accent_RecWord == noAccentDicWord: 
-        return print(dic_word, meaning) # Print the result (word and meaning) // * Grammatical class is missing
+def back_end():  # Search word in database when click in "Pesquisar"
 
-while True: # Loop search system
-    received_word = str(input('Palavra: '))
-    for fil in allFiles: # In allFiles
-        root = ET.parse(fil).getroot() # Extracts root
-        inside = root.findall('entry') # Search in <entry> tag in the xml file
+    def word_comparator():  # Compare the received word from the input and the dictionary database
+        no_accent_rec_word = unidecode(received_word).upper()  # Clean input in unicode
+        no_accent_dic_word = unidecode(dic_word).upper()  # Clean database in unicode
+        if no_accent_rec_word == no_accent_dic_word:
+            return dic_word, meaning  # Print the result (word and meaning) // * Grammatical class is missing
+
+    # Select the directory for the XML database
+    path = os.getcwd() + '/xmls'
+    all_files = glob.glob(path + '/*.xml')
+    received_word = word_entry.get()
+
+    for fil in all_files:  # In allFiles
+        root = ET.parse(fil).getroot()  # Extracts root
+        inside = root.findall('entry')  # Search in <entry> tag in the xml file
         for c in inside:
-            dic_word = c.find('form/orth').text # Get the word
+            dic_word = c.find('form/orth').text  # Get the word
             # grammar = c.find('sense/gramGrp') # Get the grammatical class // ** Missing code
-            meaning = c.find('sense/def').text # Get the meaning from the dictionary
-            word_comparator()
-    if received_word == '0': # Input 0 to close the program
-        print('Fim do programa.')
-        break     
+            meaning = c.find('sense/def').text  # Get the meaning from the dictionary
+            if word_comparator():
+                result_label['text'] = dic_word, meaning
+
+
+#
+rootwind = Tk()
+rootwind.title('Dicionário Open Source')
+#
+word_entry = Entry(rootwind)
+search_button = Button(rootwind, text='Pesquisar', command=back_end)
+quit_button = Button(rootwind, text='Fechar', command=rootwind.destroy)
+result_label = Label(rootwind, text='Nada')
+#
+rootwind.grid_rowconfigure(0, weight=1)
+rootwind.grid_rowconfigure(3, weight=1)
+rootwind.grid_columnconfigure(0, weight=1)
+rootwind.grid_columnconfigure(3, weight=1)
+#
+word_entry.grid(row=1, column=1, columnspan=2)
+search_button.grid(row=2, column=1)
+quit_button.grid(row=2, column=2)
+result_label.grid(row=3, column=1, columnspan=2)
+#
+rootwind.geometry('300x300+200+200')
+rootwind.mainloop()
+
+
+
 
 
 
